e37382d53efb224caaa5ec39a77ec5fec3e8e5a7
chenxi <chenxi20080806@126.com>
2019-09-12 16:13:15 +0800

    rpc exception filter

--- src/main/java/org/chx/commons/rpc/dubbo/filter/RpcResultCheckFilter.java
+++ src/main/java/org/chx/commons/rpc/dubbo/filter/RpcResultCheckFilter.java
@@ -7,6 +7,8 @@ import org.chx.commons.serialize.JsonHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Objects;
+
 /**
  * dubbo consumer rpc exception filter
  *
@@ -22,21 +24,26 @@ public class RpcResultCheckFilter implements Filter {
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         String refer = invoker.getInterface().getName() + "." + invocation.getMethodName();
 
+        Throwable throwable = null;
+        Result result = null;
         long s = System.currentTimeMillis();
         try {
-            Result result = invoker.invoke(invocation);
+            result = invoker.invoke(invocation);
             if (result.hasException()) {
-                log.error("refer={}||params={}", refer, JsonHelper.toJson(invocation.getArguments()), result.getException());
-                /* 抛出自定义异常 throw new SystemException(StatusCode.RPC_REQUEST_ERROR, result.getException().getMessage()); */
-                throw new RuntimeException(result.getException());
+                throwable = result.getException();
             }
+        } catch (RpcException e) {
+            throwable = e;
+        }
 
+        if (Objects.nonNull(throwable)) {
+            log.error("refer={}||params={}", refer, JsonHelper.toJson(invocation.getArguments()), throwable);
+
+            /* 抛出自定义异常 throw new SystemException(StatusCode.RPC_REQUEST_ERROR, result.getException().getMessage()); */
+            return new RpcResult(new RuntimeException(throwable));
+        } else {
             log.warn("refer={}||params={}||result={}||proc_time={}", refer, JsonHelper.toJson(invocation.getArguments()), JsonHelper.toJson(result.getValue()), System.currentTimeMillis() - s);
             return result;
-        } catch (RpcException e) {
-            log.error("refer={}||params={}", refer, JsonHelper.toJson(invocation.getArguments()), e);
-            /* 抛出自定义异常 throw new SystemException(StatusCode.RPC_REQUEST_ERROR, e.getMessage());*/
-            throw new RuntimeException(e);
         }
     }
 
